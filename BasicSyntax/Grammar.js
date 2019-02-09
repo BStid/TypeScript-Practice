b565c73b6bc4e4dcb56c2e7bb5c9c5c4ab672e18
Benjamin Stidham <benjamindstidham@gmail.com>
2019-02-09 11:38:35 -0600

    added basic syntax example

---
+++ BasicSyntax/Grammar.js
@@ -0,0 +1,7 @@
+"use strict";
+//Type Annotation
+var num = 2;
+var num2;
+//You can even use plain JavaScript
+var num3 = 3;
+var num4 = num3 + 100;
---
+++ BasicSyntax/Grammar.ts
@@ -0,0 +1,9 @@
+//Type Annotation
+
+let num: number = 2;
+let num2: number;
+
+//You can even use plain JavaScript
+
+let num3 = 3;
+let num4 = num3 + 100;
