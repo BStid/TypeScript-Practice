aec99bbbf3eddfe22f9b42b60a6128b4d57d2d54
BStid <benjamindstidham@gmail.com>
2019-07-02 10:40:58 -0500

    TypeScript Practice

---
+++ TypeScriptSandbox/types.js
@@ -0,0 +1,31 @@
+// TypeScript Type Variables
+var myString;
+var myNumber;
+var myBoolean;
+var myVar;
+myVar = 5;
+myBoolean = true;
+myNumber = 22;
+myString = "This number is " + myNumber + ".";
+// TypeScript Type Arrays
+var strArr;
+var numArr;
+var boolArr;
+var anyArr;
+strArr = ["Hello", "World"];
+numArr = [3, 4, 5];
+boolArr = [true, false, true];
+anyArr = [1, "Hello", true, myString];
+//Another Way to Define Arrays
+var newStrArr;
+var newNumArr;
+var newBoolArr;
+var newAnyArr;
+newStrArr = ["One", "Two", "Three"];
+newNumArr = [1, 2, 3];
+newBoolArr = [true, false, true];
+newAnyArr = [1, "two", false, numArr];
+//Tuple
+var strNumTuple;
+strNumTuple = ["Hello", 4];
+console.log(newAnyArr);
---
+++ TypeScriptSandbox/types.ts
@@ -0,0 +1,38 @@
+// TypeScript Type Variables
+let myString: string;
+let myNumber: number;
+let myBoolean: boolean;
+let myVar: any;
+
+myVar = 5;
+myBoolean = true;
+myNumber = 22;
+myString = `This number is ${myNumber}.`;
+
+// TypeScript Type Arrays
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[];
+let anyArr: any[];
+
+strArr = ["Hello", "World"];
+numArr = [3, 4, 5];
+boolArr = [true, false, true];
+anyArr = [1, "Hello", true, myString];
+
+//Another Way to Define Arrays
+let newStrArr: Array<string>;
+let newNumArr: Array<number>;
+let newBoolArr: Array<boolean>;
+let newAnyArr: Array<any>;
+
+newStrArr = ["One", "Two", "Three"];
+newNumArr = [1, 2, 3];
+newBoolArr = [true, false, true];
+newAnyArr = [1, "two", false, numArr];
+
+//Tuple
+let strNumTuple: [string, number];
+
+strNumTuple = ["Hello", 4];
+console.log(newAnyArr);
